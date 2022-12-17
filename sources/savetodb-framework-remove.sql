7e82b7513be1991b9f7ec929bde57d378208da3f
Sergey Vaselenko <s.vaselenko@gartle.com>
2022-12-17 14:27:42 +0300

    sources

---
+++ sources/savetodb-framework-remove.sql
@@ -0,0 +1,233 @@
+-- =============================================
+-- SaveToDB Framework for Microsoft SQL Server
+-- Version 10.6, December 13, 2022
+--
+-- This code removes SaveToDB Administrator Framework and SaveToDB Developer Framework also
+-- as the frameworks require SaveToDB Framework.
+--
+-- Copyright 2011-2022 Gartle LLC
+--
+-- License: MIT
+-- =============================================
+
+SET NOCOUNT ON
+GO
+
+-- Removing SaveToDB Developer Framework
+
+IF OBJECT_ID('[xls].[usp_translations]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[usp_translations];
+GO
+IF OBJECT_ID('[xls].[usp_translations_change]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[usp_translations_change];
+GO
+IF OBJECT_ID('[xls].[xl_actions_generate_constraints]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[xl_actions_generate_constraints];
+GO
+IF OBJECT_ID('[xls].[xl_actions_generate_handlers]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[xl_actions_generate_handlers];
+GO
+IF OBJECT_ID('[xls].[xl_actions_generate_procedures]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[xl_actions_generate_procedures];
+GO
+IF OBJECT_ID('[xls].[xl_actions_set_role_permissions]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[xl_actions_set_role_permissions];
+GO
+IF OBJECT_ID('[xls].[xl_delete_translation]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[xl_delete_translation];
+GO
+IF OBJECT_ID('[xls].[xl_export_settings]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[xl_export_settings];
+GO
+IF OBJECT_ID('[xls].[xl_import_formats]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[xl_import_formats];
+GO
+IF OBJECT_ID('[xls].[xl_import_handlers]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[xl_import_handlers];
+GO
+IF OBJECT_ID('[xls].[xl_import_objects]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[xl_import_objects];
+GO
+IF OBJECT_ID('[xls].[xl_import_translations]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[xl_import_translations];
+GO
+IF OBJECT_ID('[xls].[xl_import_workbooks]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[xl_import_workbooks];
+GO
+
+IF OBJECT_ID('[xls].[view_all_translations]', 'V') IS NOT NULL
+DROP VIEW [xls].[view_all_translations];
+GO
+IF OBJECT_ID('[xls].[view_developer_handlers]', 'V') IS NOT NULL
+DROP VIEW [xls].[view_developer_handlers];
+GO
+IF OBJECT_ID('[xls].[view_foreign_keys]', 'V') IS NOT NULL
+DROP VIEW [xls].[view_foreign_keys];
+GO
+IF OBJECT_ID('[xls].[view_framework_objects]', 'V') IS NOT NULL
+DROP VIEW [xls].[view_framework_objects];
+GO
+IF OBJECT_ID('[xls].[view_primary_keys]', 'V') IS NOT NULL
+DROP VIEW [xls].[view_primary_keys];
+GO
+IF OBJECT_ID('[xls].[view_unique_keys]', 'V') IS NOT NULL
+DROP VIEW [xls].[view_unique_keys];
+GO
+
+IF OBJECT_ID('[xls].[get_escaped_parameter_name]', 'FN') IS NOT NULL
+DROP FUNCTION [xls].[get_escaped_parameter_name];
+GO
+IF OBJECT_ID('[xls].[get_friendly_column_name]', 'FN') IS NOT NULL
+DROP FUNCTION [xls].[get_friendly_column_name];
+GO
+IF OBJECT_ID('[xls].[get_procedure_underlying_table]', 'FN') IS NOT NULL
+DROP FUNCTION [xls].[get_procedure_underlying_table];
+GO
+IF OBJECT_ID('[xls].[get_translated_string]', 'FN') IS NOT NULL
+DROP FUNCTION [xls].[get_translated_string];
+GO
+IF OBJECT_ID('[xls].[get_unescaped_parameter_name]', 'FN') IS NOT NULL
+DROP FUNCTION [xls].[get_unescaped_parameter_name];
+GO
+IF OBJECT_ID('[xls].[get_view_underlying_table]', 'FN') IS NOT NULL
+DROP FUNCTION [xls].[get_view_underlying_table];
+GO
+
+-- Removing SaveToDB Administrator Framework
+
+IF OBJECT_ID('[xls].[usp_database_permissions]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[usp_database_permissions];
+GO
+IF OBJECT_ID('[xls].[usp_database_permissions_change]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[usp_database_permissions_change];
+GO
+IF OBJECT_ID('[xls].[usp_object_permissions]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[usp_object_permissions];
+GO
+IF OBJECT_ID('[xls].[usp_object_permissions_change]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[usp_object_permissions_change];
+GO
+IF OBJECT_ID('[xls].[usp_principal_permissions]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[usp_principal_permissions];
+GO
+IF OBJECT_ID('[xls].[usp_principal_permissions_change]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[usp_principal_permissions_change];
+GO
+IF OBJECT_ID('[xls].[usp_role_members]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[usp_role_members];
+GO
+IF OBJECT_ID('[xls].[usp_role_members_change]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[usp_role_members_change];
+GO
+IF OBJECT_ID('[xls].[xl_parameter_values_principal]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[xl_parameter_values_principal];
+GO
+IF OBJECT_ID('[xls].[xl_parameter_values_schema]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[xl_parameter_values_schema];
+GO
+
+-- Removing SaveToDB Framework
+
+IF OBJECT_ID('[xls].[xl_actions_set_framework_10_mode]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[xl_actions_set_framework_10_mode];
+GO
+IF OBJECT_ID('[xls].[xl_actions_set_framework_9_mode]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[xl_actions_set_framework_9_mode];
+GO
+IF OBJECT_ID('[xls].[xl_actions_set_extended_role_permissions]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[xl_actions_set_extended_role_permissions];
+GO
+IF OBJECT_ID('[xls].[xl_actions_revoke_extended_role_permissions]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[xl_actions_revoke_extended_role_permissions];
+GO
+IF OBJECT_ID('[xls].[xl_actions_set_role_permissions]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[xl_actions_set_role_permissions];
+GO
+IF OBJECT_ID('[xls].[xl_update_table_format]', 'P') IS NOT NULL
+DROP PROCEDURE [xls].[xl_update_table_format];
+GO
+
+IF OBJECT_ID('[xls].[queries]', 'V') IS NOT NULL
+DROP VIEW [xls].[queries];
+GO
+IF OBJECT_ID('[xls].[users]', 'V') IS NOT NULL
+DROP VIEW [xls].[users];
+GO
+IF OBJECT_ID('[xls].[view_columns]', 'V') IS NOT NULL
+DROP VIEW [xls].[view_columns];
+GO
+IF OBJECT_ID('[xls].[view_formats]', 'V') IS NOT NULL
+DROP VIEW [xls].[view_formats];
+GO
+IF OBJECT_ID('[xls].[view_handlers]', 'V') IS NOT NULL
+DROP VIEW [xls].[view_handlers];
+GO
+IF OBJECT_ID('[xls].[view_objects]', 'V') IS NOT NULL
+DROP VIEW [xls].[view_objects];
+GO
+IF OBJECT_ID('[xls].[view_queries]', 'V') IS NOT NULL
+DROP VIEW [xls].[view_queries];
+GO
+IF OBJECT_ID('[xls].[view_translations]', 'V') IS NOT NULL
+DROP VIEW [xls].[view_translations];
+GO
+IF OBJECT_ID('[xls].[view_workbooks]', 'V') IS NOT NULL
+DROP VIEW [xls].[view_workbooks];
+GO
+
+IF OBJECT_ID('[xls].[columns]', 'U') IS NOT NULL
+DROP TABLE [xls].[columns];
+GO
+IF OBJECT_ID('[xls].[formats]', 'U') IS NOT NULL
+DROP TABLE [xls].[formats];
+GO
+IF OBJECT_ID('[xls].[handlers]', 'U') IS NOT NULL
+DROP TABLE [xls].[handlers];
+GO
+IF OBJECT_ID('[xls].[objects]', 'U') IS NOT NULL
+DROP TABLE [xls].[objects];
+GO
+IF OBJECT_ID('[xls].[translations]', 'U') IS NOT NULL
+DROP TABLE [xls].[translations];
+GO
+IF OBJECT_ID('[xls].[workbooks]', 'U') IS NOT NULL
+DROP TABLE [xls].[workbooks];
+GO
+
+
+DECLARE @sql nvarchar(max) = ''
+
+SELECT
+    @sql = @sql + 'ALTER ROLE ' + QUOTENAME(r.name) + ' DROP MEMBER ' + QUOTENAME(m.name) + ';' + CHAR(13) + CHAR(10)
+FROM
+    sys.database_role_members rm
+    INNER JOIN sys.database_principals r ON r.principal_id = rm.role_principal_id
+    INNER JOIN sys.database_principals m ON m.principal_id = rm.member_principal_id
+WHERE
+    r.name IN ('xls_admins', 'xls_developers', 'xls_formats', 'xls_users')
+
+IF LEN(@sql) > 1
+    BEGIN
+    EXEC (@sql);
+    PRINT @sql
+    END
+GO
+
+IF DATABASE_PRINCIPAL_ID('xls_admins') IS NOT NULL
+DROP ROLE [xls_admins];
+GO
+IF DATABASE_PRINCIPAL_ID('xls_developers') IS NOT NULL
+DROP ROLE [xls_developers];
+GO
+IF DATABASE_PRINCIPAL_ID('xls_formats') IS NOT NULL
+DROP ROLE [xls_formats];
+GO
+IF DATABASE_PRINCIPAL_ID('xls_users') IS NOT NULL
+DROP ROLE [xls_users];
+GO
+
+IF SCHEMA_ID('xls') IS NOT NULL
+DROP SCHEMA [xls];
+GO
+
+print 'SaveToDB Framework removed';
---
+++ sources/savetodb-framework-update.sql
@@ -0,0 +1,132 @@
+-- =============================================
+-- SaveToDB Framework for SQL Server
+-- Version 10.6, December 13, 2022
+--
+-- This script updates SaveToDB Framework 10 to the latest version
+--
+-- Copyright 2011-2022 Gartle LLC
+--
+-- License: MIT
+-- =============================================
+
+IF 1006 <= COALESCE((SELECT CAST(LEFT(HANDLER_CODE, CHARINDEX('.', HANDLER_CODE) - 1) AS int) * 100 + CAST(RIGHT(HANDLER_CODE, LEN(HANDLER_CODE) - CHARINDEX('.', HANDLER_CODE)) AS float) FROM xls.handlers WHERE TABLE_SCHEMA = 'xls' AND TABLE_NAME = 'savetodb_framework' AND COLUMN_NAME = 'version' AND EVENT_NAME = 'Information'), 0)
+    RAISERROR('SaveToDB Framework is up-to-date. Update skipped', 11, 0)
+GO
+
+DELETE FROM xls.handlers WHERE TABLE_SCHEMA = 'xls' AND TABLE_NAME = 'users' AND HANDLER_NAME = 'xl_actions_set_role_permissions';
+GO
+
+UPDATE xls.handlers
+SET
+    HANDLER_TYPE = s.HANDLER_TYPE
+    , HANDLER_CODE = s.HANDLER_CODE
+    , TARGET_WORKSHEET = s.TARGET_WORKSHEET
+    , MENU_ORDER = s.MENU_ORDER
+    , EDIT_PARAMETERS = s.EDIT_PARAMETERS
+FROM
+    (
+    SELECT
+        CAST(NULL AS nvarchar) AS TABLE_SCHEMA
+        , CAST(NULL AS nvarchar) AS TABLE_NAME
+        , CAST(NULL AS nvarchar) AS COLUMN_NAME
+        , CAST(NULL AS nvarchar) AS EVENT_NAME
+        , CAST(NULL AS nvarchar) AS HANDLER_SCHEMA
+        , CAST(NULL AS nvarchar) AS HANDLER_NAME
+        , CAST(NULL AS nvarchar) AS HANDLER_TYPE
+        , CAST(NULL AS nvarchar) HANDLER_CODE
+        , CAST(NULL AS nvarchar) AS TARGET_WORKSHEET
+        , CAST(NULL AS int) AS MENU_ORDER
+        , CAST(NULL AS bit) AS EDIT_PARAMETERS
+
+    UNION ALL SELECT N'xls', N'savetodb_framework', N'version', N'Information', NULL, NULL, N'ATTRIBUTE', N'10.6', NULL, NULL, NULL
+    UNION ALL SELECT N'xls', N'handlers', N'EVENT_NAME', N'ValidationList', NULL, NULL, N'VALUES', N'Actions, AddHyperlinks, AddStateColumn, Authentication, BitColumn, Change, ContextMenu, ConvertFormulas, DataTypeBit, DataTypeBoolean, DataTypeDate, DataTypeDateTime, DataTypeDateTimeOffset, DataTypeDouble, DataTypeInt, DataTypeGuid, DataTypeString, DataTypeTime, DataTypeTimeSpan, DefaultListObject, DefaultValue, DependsOn, DoNotAddChangeHandler, DoNotAddDependsOn, DoNotAddManyToMany, DoNotAddValidation, DoNotChange, DoNotConvertFormulas, DoNotKeepComments, DoNotKeepFormulas, DoNotSave, DoNotSelect, DoNotSort, DoNotTranslate, DoubleClick, DynamicColumns, Format, Formula, FormulaValue, Information, JsonForm, KeepFormulas, KeepComments, License, LoadFormat, ManyToMany, ParameterValues, ProtectRows, RegEx, SaveFormat, SaveWithoutTransaction, SelectionChange, SelectionList, SelectPeriod, SyncParameter, UpdateChangedCellsOnly, UpdateEntireRow, ValidationList', NULL, NULL, NULL
+
+    ) s
+    LEFT OUTER JOIN xls.handlers t ON
+        t.TABLE_SCHEMA = s.TABLE_SCHEMA
+        AND t.TABLE_NAME = s.TABLE_NAME
+        AND COALESCE(t.COLUMN_NAME, '') = COALESCE(s.COLUMN_NAME, '')
+        AND t.EVENT_NAME = s.EVENT_NAME
+        AND COALESCE(t.HANDLER_SCHEMA, '') = COALESCE(s.HANDLER_SCHEMA, '')
+        AND COALESCE(t.HANDLER_NAME, '') = COALESCE(s.HANDLER_NAME, '')
+WHERE
+    NOT COALESCE(t.HANDLER_TYPE, '') = COALESCE(s.HANDLER_TYPE, '')
+    OR NOT COALESCE(t.HANDLER_CODE, '')  = COALESCE(s.HANDLER_CODE, '')
+    OR NOT COALESCE(t.TARGET_WORKSHEET, '') = COALESCE(s.TARGET_WORKSHEET, '')
+    OR NOT COALESCE(t.MENU_ORDER, -1) = COALESCE(s.MENU_ORDER, -1)
+    OR NOT COALESCE(t.EDIT_PARAMETERS, 0) = COALESCE(s.EDIT_PARAMETERS, 0);
+GO
+
+INSERT INTO xls.handlers (TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, EVENT_NAME, HANDLER_SCHEMA, HANDLER_NAME, HANDLER_TYPE, HANDLER_CODE, TARGET_WORKSHEET, MENU_ORDER, EDIT_PARAMETERS)
+SELECT s.TABLE_SCHEMA, s.TABLE_NAME, s.COLUMN_NAME, s.EVENT_NAME, s.HANDLER_SCHEMA, s.HANDLER_NAME, s.HANDLER_TYPE, s.HANDLER_CODE, s.TARGET_WORKSHEET, s.MENU_ORDER, s.EDIT_PARAMETERS
+FROM
+    (
+    SELECT
+        CAST(NULL AS nvarchar) AS TABLE_SCHEMA
+        , CAST(NULL AS nvarchar) AS TABLE_NAME
+        , CAST(NULL AS nvarchar) AS COLUMN_NAME
+        , CAST(NULL AS nvarchar) AS EVENT_NAME
+        , CAST(NULL AS nvarchar) AS HANDLER_SCHEMA
+        , CAST(NULL AS nvarchar) AS HANDLER_NAME
+        , CAST(NULL AS nvarchar) AS HANDLER_TYPE
+        , CAST(NULL AS nvarchar) HANDLER_CODE
+        , CAST(NULL AS nvarchar) AS TARGET_WORKSHEET
+        , CAST(NULL AS int) AS MENU_ORDER
+        , CAST(NULL AS bit) AS EDIT_PARAMETERS
+
+    UNION ALL SELECT N'xls', N'savetodb_framework', N'version', N'Information', NULL, NULL, N'ATTRIBUTE', N'10.6', NULL, NULL, NULL
+    UNION ALL SELECT N'xls', N'handlers', N'EVENT_NAME', N'ValidationList', NULL, NULL, N'VALUES', N'Actions, AddHyperlinks, AddStateColumn, Authentication, BitColumn, Change, ContextMenu, ConvertFormulas, DataTypeBit, DataTypeBoolean, DataTypeDate, DataTypeDateTime, DataTypeDateTimeOffset, DataTypeDouble, DataTypeInt, DataTypeGuid, DataTypeString, DataTypeTime, DataTypeTimeSpan, DefaultListObject, DefaultValue, DependsOn, DoNotAddChangeHandler, DoNotAddDependsOn, DoNotAddManyToMany, DoNotAddValidation, DoNotChange, DoNotConvertFormulas, DoNotKeepComments, DoNotKeepFormulas, DoNotSave, DoNotSelect, DoNotSort, DoNotTranslate, DoubleClick, DynamicColumns, Format, Formula, FormulaValue, Information, JsonForm, KeepFormulas, KeepComments, License, LoadFormat, ManyToMany, ParameterValues, ProtectRows, RegEx, SaveFormat, SaveWithoutTransaction, SelectionChange, SelectionList, SelectPeriod, SyncParameter, UpdateChangedCellsOnly, UpdateEntireRow, ValidationList', NULL, NULL, NULL
+
+    ) s
+    LEFT OUTER JOIN xls.handlers t ON
+        t.TABLE_SCHEMA = s.TABLE_SCHEMA
+        AND t.TABLE_NAME = s.TABLE_NAME
+        AND COALESCE(t.COLUMN_NAME, '') = COALESCE(s.COLUMN_NAME, '')
+        AND t.EVENT_NAME = s.EVENT_NAME
+        AND COALESCE(t.HANDLER_SCHEMA, '') = COALESCE(s.HANDLER_SCHEMA, '')
+        AND COALESCE(t.HANDLER_NAME, '') = COALESCE(s.HANDLER_NAME, '')
+        AND COALESCE(t.HANDLER_TYPE, '') = COALESCE(s.HANDLER_TYPE, '')
+WHERE
+    t.TABLE_NAME IS NULL
+    AND s.TABLE_NAME IS NOT NULL;
+GO
+
+IF (SELECT ROUTINE_NAME FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_SCHEMA = 'xls' AND ROUTINE_NAME = 'xl_actions_set_role_permissions') IS NULL
+EXEC('CREATE PROCEDURE [xls].[xl_actions_set_role_permissions] AS SET NOCOUNT ON;')
+GO
+
+-- =============================================
+-- Author:      Gartle LLC
+-- Release:     10.4, 2022-10-13
+-- Description: The procedure sets permissions of the SaveToDB Framework roles
+-- =============================================
+
+ALTER PROCEDURE [xls].[xl_actions_set_role_permissions]
+    @confirm bit = 0
+AS
+BEGIN
+
+SET NOCOUNT ON;
+
+IF COALESCE(@confirm, 0) = 0 RETURN;
+
+REVOKE SELECT, EXECUTE, VIEW DEFINITION ON SCHEMA::xls TO xls_users;
+
+GRANT SELECT, VIEW DEFINITION ON xls.columns        TO xls_users;
+GRANT SELECT, VIEW DEFINITION ON xls.formats        TO xls_users;
+GRANT SELECT, VIEW DEFINITION ON xls.handlers       TO xls_users;
+GRANT SELECT, VIEW DEFINITION ON xls.objects        TO xls_users;
+GRANT SELECT, VIEW DEFINITION ON xls.translations   TO xls_users;
+GRANT SELECT, VIEW DEFINITION ON xls.workbooks      TO xls_users;
+GRANT SELECT, VIEW DEFINITION ON xls.queries        TO xls_users;
+
+GRANT SELECT, INSERT, UPDATE, DELETE, VIEW DEFINITION ON xls.formats TO xls_formats;
+
+GRANT SELECT, EXECUTE, INSERT, UPDATE, DELETE, VIEW DEFINITION ON SCHEMA::xls   TO xls_developers;
+
+END
+
+
+GO
+
+print 'SaveToDB Framework updated';
